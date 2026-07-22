e63199439c004506b22158819562fe4105041897
quyetnguyen02 <quyetnguyen0804@gmail.com>
2026-07-22 16:46:18 +0000

    add favicon.ico

--- resources/views/UserPage/layouts/app.blade.php
+++ resources/views/UserPage/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <title>@yield('title', 'BOSHUN')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
